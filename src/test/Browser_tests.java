8274e8cad6b177715945c23ed4cb911528a07440
Paul Puehringer <paul.puehringer@outlook.com>
2021-06-20 13:37:35 +0200

    chrome headless mode

--- src/test/Browser_tests.java
+++ src/test/Browser_tests.java
@@ -15,7 +15,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -35,7 +37,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
     private String baseUrl = "http://www.amazon.de";
     private String browser;
 
-    @Before public void setUp() throws Exception {
+    @Before public void setUp() {
         if (browser.equals("edge")) {
             System.setProperty("webdriver.edge.driver", "C:/Program Files (x86)/Microsoft/Edge/Application/msedgedriver.exe");
             DesiredCapabilities capabilities = DesiredCapabilities.edge();
@@ -44,10 +46,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
             driver = new EdgeDriver(capabilities);
         } else
        if (browser.equals("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "C:/Program Files/Google/chromedriver.exe");
-            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-            capabilities.setCapability("test", true);
-            driver = new ChromeDriver(capabilities);
+           System.setProperty("webdriver.chrome.driver", "C:/Program Files/Google/chromedriver.exe");
+           ChromeOptions options = new ChromeOptions();
+           options.addArguments("--headless", "--disable-gpu");
+           driver = new ChromeDriver(options);
         }
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
@@ -59,18 +61,22 @@ import org.openqa.selenium.support.ui.WebDriverWait;
         new WebDriverWait(driver, 10)
                 .until(ExpectedConditions.attributeContains(By.id("nav-flyout-searchAjax"), "style", "display: block;"));
 
+        //arrow down one time to select 'bürostuhl grau'
         for(int i = 0; i<1;i++){
             driver.findElement(By.id("twotabsearchtextbox")).sendKeys(Keys.ARROW_DOWN);
         }
+
+        //press enter to choose 'bürostuhl grau'
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys(Keys.ENTER);
 
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.urlContains("k=b%C3%BCrostuhl+grau"));
-        System.out.println(driver.getCurrentUrl());
+//        new WebDriverWait(driver, 10)
+//                .until(ExpectedConditions.urlContains("k=b%C3%BCrostuhl+grau"));
+//        System.out.println(driver.getCurrentUrl());
+
         assertTrue(driver.getCurrentUrl().contains("k=b%C3%BCrostuhl+grau"));
     }
 
-    @After public void tearDown() throws Exception {
+    @After public void tearDown() {
         driver.quit();
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
